c164ac740c207859e194474656eefd04fef2d518
m2015HW <m2015@hw.ac.uk>
2023-02-26 13:15:54 +0000

    Phase Class of GUI
    in this class, I declared phase number phase duration as integer. I have defined getter and setters for that. Now we can map out vehicle class with phase class.

---
+++ src/Phase.java
@@ -0,0 +1,18 @@
+public class Phase {
+
+    private int phaseNumber;
+    private int phaseDuration;
+
+    public Phase(int phaseNumber, int phaseDuration) {
+        this.phaseNumber = phaseNumber;
+        this.phaseDuration = phaseDuration;
+    }
+
+    public int getPhaseNumber() {
+        return phaseNumber;
+    }
+
+    public int getPhaseDuration() {
+        return phaseDuration;
+    }
+}
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,11 +1,63 @@
-/**
- * 
- */
+enum crossingStatus{
+        CROSSED, NOTCROSSED
+}
+enum vehicleDirection {
+    STRAIGHT,
+    RIGHT,
+    LEFT
+}
+class Vehicle {
+    private String plateId;
+    private String segment;
+    private String type ;
+    private int crossingTime;
+    private vehicleDirection direction;
+    private crossingStatus status;
+    private int length;
+    private int emission;
 
-/**
- * @author Vikas
- *
- */
-public class Vehicle {
+    public Vehicle(String segment, String plateId, String type, int crossingTime,
+                   vehicleDirection direction,
+                   int length, int emission, crossingStatus status) {
+        this.plateId = plateId;
+        this.segment = segment;
+        this.type = type;
+        this.crossingTime = crossingTime;
+        this.direction = direction;
+        this.status = status;
+        this.length = length;
+        this.emission = emission;
+    }
 
+    public String getPlateId() {
+        return plateId;
+    }
+
+    public String getSegment() {
+        return segment;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCrossingTime() {
+        return crossingTime;
+    }
+
+    public vehicleDirection getDirection() {
+        return direction;
+    }
+
+    public crossingStatus getStatus() {
+        return status;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getEmission() {
+        return emission;
+    }
 }
