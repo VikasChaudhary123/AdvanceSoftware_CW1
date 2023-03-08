15d81ef2b76d1055e425d9e76a7ebca8a56b36d5
Jaga-droid <pjagatheesh45@gmail.com>
2023-03-08 00:14:56 +0000

    GUI table cell changes
    Disable cell editing for all tables except the one for adding a vehicle

--- src/GUIClass.java
+++ src/GUIClass.java
@@ -24,6 +24,7 @@ public GUIClass(VehicleList vehicleList) {
 	vehicleTable = table.vehicleModel();	
 	vehicleTable.setBounds(30,40,80,80); 
 	vehicleTable.getTableHeader().setReorderingAllowed(false);
+	vehicleTable.setEnabled(false);
 	JScrollPane sp=new JScrollPane(vehicleTable);   
 	add(sp,BorderLayout.NORTH);
 	
@@ -40,6 +41,7 @@ public GUIClass(VehicleList vehicleList) {
     segmentsummaryTable= new JTable(segmentsummarymodel);
     segmentsummaryTable.setBounds(30,40,80,80); 
     segmentsummaryTable.getTableHeader().setReorderingAllowed(false);
+    segmentsummaryTable.setEnabled(false);
     JScrollPane scrollSegment=new JScrollPane(segmentsummaryTable);   
     add(scrollSegment,BorderLayout.EAST);
     
@@ -48,6 +50,7 @@ public GUIClass(VehicleList vehicleList) {
     phaseTable= new JTable(phasemodel);
     phaseTable.setBounds(60,80,40,40); 
     phaseTable.getTableHeader().setReorderingAllowed(false);
+    phaseTable.setEnabled(false);
     JScrollPane scrollPhase=new JScrollPane(phaseTable);   
 	add(scrollPhase,BorderLayout.WEST);
 	
@@ -92,19 +95,7 @@ public void actionPerformed(ActionEvent e) {
 	
 	      if(e.getSource()==exitbutton)
 	      {
-			System.out.println("Exit button has been clicked");
-			File file = new File("PhaseSummary.txt");
-			FileWriter fw;
-			try {
-				fw = new FileWriter(file.getAbsoluteFile());
-				BufferedWriter bw = new BufferedWriter(fw);
-				bw.write("Phase Summary");
-				bw.close();
-			} 
-			catch (IOException e1) {
-				
-				e1.printStackTrace();
-			}
+			vehicleList.PhaseSummary("PhaseSummary.txt");
 	      }
 	      if(e.getSource()==addbutton) {
 	    	  System.out.println("Add button clicked");
@@ -150,6 +141,7 @@ public void actionPerformed(ActionEvent e) {
 			catch (NumberFormatException e1) {
 				JOptionPane.showMessageDialog(null,e1.getMessage(),"Number Format Exception",
 				        JOptionPane.INFORMATION_MESSAGE);
+				
 			}
 			catch (IllegalArgumentException e1) {
 				JOptionPane.showMessageDialog(null,e1.getMessage(),"Illegal Argument Exception",
