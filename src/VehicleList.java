20b5c0dd49689ef1721ec1b867673dfceedb9659
kl2063 <kl2063@hw.ac.uk>
2023-03-10 11:52:55 +0000

    Update Manager ad VehicleList
    1. Remove code

--- src/VehicleList.java
+++ src/VehicleList.java
@@ -7,8 +7,6 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import org.junit.Assert;
-import org.junit.Test;
 import java.util.*;
 
 public class VehicleList {
